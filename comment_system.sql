9c2e8499d74b39262184cdf50dfd919e1e8051a7
DESKTOP-DPH72HB\Eric <er@telecom-object.com>
2017-08-29 20:32:57 +0400

    update database schema

--- comment_system.sql
+++ comment_system.sql
@@ -1,55 +1,62 @@
--- --------------------------------------------------------
--- Hôte :                        127.0.0.1
--- Version du serveur:           10.0.32-MariaDB - mariadb.org binary distribution
--- SE du serveur:                Win64
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 29 Août 2017 à 16:29
+-- Version du serveur :  10.0.32-MariaDB
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `comment_system`
+--
 
--- Export de la structure de la base pour comment_system
-DROP DATABASE IF EXISTS `comment_system`;
-CREATE DATABASE IF NOT EXISTS `comment_system` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `comment_system`;
+-- --------------------------------------------------------
 
--- Export de la structure de la table comment_system. comments
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `user` varchar(64) NOT NULL,
   `text` text NOT NULL,
   `date` datetime NOT NULL,
   `post_id` int(11) NOT NULL,
-  KEY `FK1` (`post_id`),
-  CONSTRAINT `FK1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+  `is_spam` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
--- Export de la structure de la table comment_system. posts
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE IF NOT EXISTS `posts` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `author` text NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
--- Export de la structure de la table comment_system. spam_comments
-DROP TABLE IF EXISTS `spam_comments`;
-CREATE TABLE IF NOT EXISTS `spam_comments` (
-  `id` int(11) NOT NULL,
-  `user` varchar(64) NOT NULL,
-  `text` text NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Index pour les tables exportées
+--
 
--- Les données exportées n'étaient pas sélectionnées.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
